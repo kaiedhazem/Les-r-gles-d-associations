20b70d5c2a35bcb3ad133e45f3f79ad591ec2049
unknown <gaiedhazem@gmail.com>
2022-11-21 22:02:04 +0100

    final commit

--- main.py
+++ main.py
@@ -3,37 +3,34 @@ import pandas as pd
 from mlxtend.frequent_patterns import apriori 
 from mlxtend.frequent_patterns import association_rules
 import numpy as np
-# read text file into pandas DataFrame
-df = pd.read_csv('market_basket.txt', 
-                 header=None,delim_whitespace=True,
-                 names='ID Product'.split(' '))
-df1 = df.head(10)
+# lire fichier texte avec pandas DataFrame
+df = pd.read_csv('market_basket.txt', header=None,delim_whitespace=True,names='ID Product'.split(' '))
 # Afficher les 10 premières lignes du DataFrame
-#print(df1)
+df1 = df.head(10)
+print(df1)
 # Afficher les dimensions du dataframe.
-#print(df.shape)
-
+print(df.shape)
 #extraire les valeurs sans redondances
 headers = df1["ID"].drop_duplicates()
 products = df1["Product"].drop_duplicates()
 # compare result with crosstab
 T=pd.crosstab(df["ID"], [df["Product"]], rownames=['ID'], colnames=['Products'])
-#print(T.iloc[:30,:3])
-
-#a priori
+print(T.iloc[:30,:3])
+# fonction a priori
 freq_itemsets = apriori(T,min_support=0.025,max_len=4,use_colnames=True)
-#print(freq_itemsets.head(15))
+print(freq_itemsets.head(15))
 #fonction de test d'inclusion
 def is_inclus(x,items):
  return items.issubset(x)
-
+#preparation des itemsets qui contient 'Aspirin' et 'Aspirin , Eggs' 
 id = np.where(freq_itemsets.itemsets.apply(is_inclus,items={'Aspirin'}))
 id2 = np.where(freq_itemsets.itemsets.apply(is_inclus,items={'Aspirin','Eggs'}))
 #affichage des itemsets corresp.
-#print(freq_itemsets.loc[id2])
+print(freq_itemsets.loc[id])
+print(freq_itemsets.loc[id2])
 regles = association_rules(freq_itemsets,metric="confidence",min_threshold=0.75)
-#5 "premières" règles
-#print(regles.iloc[:5,:])
+#afficher 5 "premières" règles
+print(regles.iloc[:5,:])
 #affichage des règles avec un LIFT supérieur ou égal à 7
 print(regles[regles['lift'].ge(7.0)])
 #filtrer les règles menant au conséquent {‘2pct_milk’}
